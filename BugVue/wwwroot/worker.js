6ae795e731ca400225c18499e0267f5850ed333c
Hakim Ghods <ghods.hakim@hotmail.com>
2018-12-25 00:00:32 +0330

    Add project files.

---
+++ BugVue/wwwroot/worker.js
@@ -0,0 +1,33 @@
+﻿self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('v1').then(function (cache) {
+            return cache.addAll([
+                '/lib/twitter-bootstrap/css/bootstrap.min.css',
+                '/lib/twitter-bootstrap/css/bootstrap-theme.min.css',
+                '/lib/toastr.js/toastr.css',
+                '/lib/jquery/jquery.min.js',
+                '/lib/twitter-bootstrap/js/bootstrap.min.js',
+                '/lib/toastr.js/toastr.min.js',
+                '/lib/vue/vue.min.js',
+                '/lib/localforage/localforage.js',
+                '/bug',
+                '/'
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function (event) {
+    console.log('[Service Worker] Fetching something ....', event);
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    return self.clients.claim();
+});
